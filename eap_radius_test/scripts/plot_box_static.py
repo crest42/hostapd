8131bc650ee1bc6ea13e4dee61d5d3d2efd0fe3c
Robin <robin@chilio.net>
2020-10-29 17:14:18 +0100

    Add new plot

--- eap_radius_test/scripts/plot_box_static.py
+++ eap_radius_test/scripts/plot_box_static.py
@@ -1,10 +1,14 @@
 from plot import *
 
-msg_cb = add_sec_level(pd.read_pickle('./pkl/msg_cb_sparse.pkl'),'algo')
-msg_cb_static = add_sec_level(pd.read_pickle('./pkl/msg_cb_static.pkl'),'algo')
+msg_cb = add_sec_level(msg_cb,'algo')
+msg_cb_static = add_sec_level(pd.read_pickle('./pkl/msg_cb_static_zero_overhead.pkl'),'algo')
 
 msg_cb_static = msg_cb_static[msg_cb_static['sec_level'] == 3]
+msg_cb_static = msg_cb_static[msg_cb_static['hybrid'] == False]
+msg_cb_static = msg_cb_static[msg_cb_static['algo'] != 'P-384']
 msg_cb = msg_cb[msg_cb['sec_level'] == 3]
+msg_cb = msg_cb[msg_cb['hybrid'] == False]
+msg_cb = msg_cb[msg_cb['algo'] != 'P-384']
 
 msg_cb_last = msg_cb[['rnd', 'algo', 'time_abs', 'clock_abs',  'clock']].groupby('rnd').last()
 msg_cb_static_last = msg_cb_static[['rnd', 'algo', 'time_abs', 'clock_abs', 'clock']].groupby('rnd').last()
@@ -13,9 +17,12 @@ msg_cb_static_last['static'] = True
 plot_df = pd.concat([msg_cb_static_last, msg_cb_last])
 
 
-order = plot_df.groupby(['algo']).mean().reset_index().sort_values('clock')['algo']
+order = plot_df.groupby(['algo']).mean().reset_index().sort_values('clock_abs')['algo']
 
-sns.boxplot(data=plot_df, y='algo', x='clock', hue='static' , order = order)
+sns.boxplot(data=plot_df, y='algo', x='clock_abs', hue='static' , order = order)
 #plt.show()
 plt.tight_layout()
 savefig(__file__)
+res = ttest(msg_cb_static_last, msg_cb_last, 'clock_abs')
+res = {k: v for k, v in sorted(res.items(), key=lambda item: item[1].pvalue)}
+
