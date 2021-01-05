71266ec6105728d062ea166cdb17df3a17fc2848
Robin <robin@chilio.net>
2021-01-05 21:22:13 +0100

    fix plot_bar_traffic2 axis labels

--- eap_radius_test/scripts/plot_bar_traffic2.py
+++ eap_radius_test/scripts/plot_bar_traffic2.py
@@ -3,8 +3,9 @@ from plot import *
 merge = merge[merge['hybrid'] == False]
 cap_size_df = merge.groupby(['algo','run']).sum().reset_index()[['algo','run','frame_len', 'rad_len', 'rad_avp_len', 'eap.len', 'tls.record.length']].melt(id_vars=['algo','run'])
 order = cap_size_df[cap_size_df['variable'] == 'frame_len'].groupby('algo').mean().reset_index().sort_values('value')['algo']
-sns.barplot(data=cap_size_df.sort_values('value'), y = 'algo', x = 'value', hue='variable', order=order)
+ax = sns.barplot(data=cap_size_df.sort_values('value'), y = 'algo', x = 'value', hue='variable', order=order)
+ax.set(xlabel='Sum of Traffic in Bytes', ylabel='Algorithm')
 plt.tight_layout()
 savefig(__file__)
-plt.show()
+#plt.show()
 
